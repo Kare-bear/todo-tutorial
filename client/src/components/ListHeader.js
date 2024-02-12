e1aa432a69734f1a6ed9cb0fa01db24015fa9e7f
Kara Rainey <kara@brivity.com>
2024-02-12 01:06:22 -0800

    added more styling to the ListHeader component in index.css and added buttons for add new and signout.

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,9 +1,18 @@
 import React from "react"
 
 const ListHeader = ({listName}) => {
+
+  const signOut = () => {
+    console.log('Signout')
+  }
+
   return(
     <div className="list-header">
       <h1>{listName}</h1>
+      <div className="button-container">
+        <button className="create">ADD NEW</button>
+        <button className="signout" onClick={signOut}>SIGN OUT</button>
+      </div>
     </div>
   )
 }
